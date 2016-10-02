a3ea8a383a8943a07262b989e9ab552ba27b3bfc
Hesam <hesamp@kth.se>
2016-10-02 18:46:08 +0200

    O(N^2) NN implemented

--- City.cpp
+++ City.cpp
@@ -8,13 +8,16 @@ class City{
     private:
         float x;
         float y;
+        int vertex;
     public:
-        City(float x, float y){
+        City(int vertex, float x, float y){
+            this -> vertex = vertex;
             this -> x = x;
             this -> y = y;
         }
         float getX(){ return x;}
         float getY(){ return y;}
+        float getVertexNum(){ return vertex;}
         float dist(City neighbor){
             float xdist = neighbor.getX() - x;
             float ydist = neighbor.getY() - y;
--- NN.cpp
+++ NN.cpp
@@ -1,10 +1,11 @@
 #include <fstream>
 #include <iostream>
+#include <stdlib.h>
+#include <time.h>
 #include <vector>
 #include "City.cpp"
 
 using namespace std;
-
 
 vector<City> buildNodes(){
     ifstream reader("./samples/tsp.in");
@@ -17,13 +18,44 @@ vector<City> buildNodes(){
             int index = line.find(" ", 0);
             float x = stod(line.substr(0,index));
             float y = stod(line.substr(index,line.length()));
-            City city(x,y);
+            City city(i,x,y);
             cities.push_back(city);
         }
         reader.close();
     return cities;
 }
 
+void nearestNeighbor(vector<City> cities){
+    vector<int> tour;
+
+    srand (time(NULL));
+    int rnd = rand() % cities.size();
+    City current = cities[rnd];
+    tour.push_back(current.getVertexNum());
+    cities.erase(cities.begin() + rnd);
+
+    float best = 99999;
+    int best_ind = -1;
+
+    while(cities.size() > 0){
+        for(int i=0; i!=cities.size(); i++){
+            float dist = current.dist(cities[i]);
+            if (dist < best){
+                best = dist;
+                best_ind = i;
+            }
+        }
+        current = cities[best_ind];
+        cities.erase(cities.begin() + best_ind);
+        tour.push_back(current.getVertexNum());
+        best = 99999;
+    }
+
+
+    for(int i=0; i!=tour.size(); i++){
+        cout << tour[i] << endl;
+    }
+}
 
 
 
@@ -31,5 +63,6 @@ vector<City> buildNodes(){
 
 int main() {
     vector<City> cities = buildNodes();
+    nearestNeighbor(cities);
     return 0;
 }
