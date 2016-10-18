c47a852d6d9edad093211f755606a2b827254f2c
Hesam <hesamp@kth.se>
2016-10-18 19:34:48 +0200

    guards in .h files

--- City.h
+++ City.h
@@ -1,9 +1,12 @@
+#ifndef __City_H_INCLUDED__
+#define __City_H_INCLUDED__
 class City{
     private:
         // store coordinates
         float x;
         float y;
         int vertex; // vertex num
+        vector<int> nearest;
     public:
         City(int vertex, float x, float y);
         float getX();
--- NearestNeighbor.h
+++ NearestNeighbor.h
@@ -1,5 +1,5 @@
-#ifndef __City_H_INCLUDED__
-#define __City_H_INCLUDED__
+#ifndef __nearestNeighbor_H_INCLUDED__
+#define __nearestNeighbor_H_INCLUDED__
 #include "City.h"
 using namespace std;
 vector<int> nearestNeighbor(vector<City> cities);
