c95807d954dd56eb161e1917b0b89e1177a22d16
tarvedson <tilde.arvedson@gmail.com>
2016-11-01 21:55:15 +0100

    NN faster

--- NearestNeighbor.cpp
+++ NearestNeighbor.cpp
@@ -14,7 +14,7 @@ using namespace std;
 
 vector<int> nearestNeighbor(Cities &cities){
 
-    //clock_t begin = clock();
+    clock_t begin = clock();
 
     vector<int> tour;
     int N = cities.getNumCities();
@@ -25,16 +25,67 @@ vector<int> nearestNeighbor(Cities &cities){
     //cout<<thisvertex<<endl;
 
     // put in the first city and delete it from cities
+   // tour.push_back(thisvertex);
+
+
+    // some variables to keep track of best
+    double best = numeric_limits<double>::max();
+    //int best_ind = -1;
+
+    bool used[N];
+
+    for(int i = 0; i < N; ++i){
+        used[i] = false;
+        tour.push_back(0);
+    }
+    used[0] = true;
+    for(int i = 1; i < N; ++i){
+        int best_ind = -1;
+        best = numeric_limits<double>::max();
+        //int best = numeric_limits<int>::max();
+        for(int j=0; j < N; ++j){
+            // calc distance between thisvertex and j
+            if(used[j] ==false){
+            //if( ( find(tour.begin(), tour.end(), j) == tour.end() )){
+                int thisdist = cities.ds(tour[i-1], j);
+                if (thisdist < best || best_ind == -1){
+                    best = thisdist;
+                    best_ind = j;
+                }
+            }
+        }
+        //if(best_ind == -1){best_ind = 0;}
+        tour[i] = best_ind;
+        used[best_ind] = true;
+    }
+    
+    clock_t end = clock();
+    double elapsed_secs = double(end - begin) / CLOCKS_PER_SEC;
+    cout<<"time after NN = "<<elapsed_secs<<endl;
+    return tour;
+}
+
+/*vector<int> nearestNeighbor(Cities &cities){
+
+    clock_t begin = clock();
+
+    vector<int> tour;
+    int N = cities.getNumCities();
+
+    // take some random vertex
+    srand (time(NULL));
+    int thisvertex = 0;//rand() % cities.getNumCities();
+    //cout<<thisvertex<<endl;
+
+    // put in the first city and delete it from cities
     tour.push_back(thisvertex);
 
 
     // some variables to keep track of best
-    int best = numeric_limits<int>::max();
+    double best = numeric_limits<double>::max();
     //int best_ind = -1;
 
-    int i = 1;
-
-    while( i < N ){
+    while( tour.size() < N ){
         int best_ind = -1;
         for(int j=0; j < N; ++j){
             // calc distance between thisvertex and j
@@ -46,15 +97,16 @@ vector<int> nearestNeighbor(Cities &cities){
                 }
             }
         }
-        ++i;
         //cout<<best_ind<<endl;
         //if(best_ind == -1){best_ind = 0;}
         thisvertex = best_ind;
         tour.push_back(thisvertex);
-        best = numeric_limits<int>::max();
+        best = numeric_limits<double>::max();
     }
-    //clock_t end = clock();
-    //double elapsed_secs = double(end - begin) / CLOCKS_PER_SEC;
-    //cout<<"time after NN = "<<elapsed_secs<<endl;
+    clock_t end = clock();
+    double elapsed_secs = double(end - begin) / CLOCKS_PER_SEC;
+    cout<<"time after NN = "<<elapsed_secs<<endl;
     return tour;
-}
+}*/
+
+
