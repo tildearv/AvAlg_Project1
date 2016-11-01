cb6fa100c7c8c95f20df9ef87dd261ab3a92feb6
tarvedson <tilde.arvedson@gmail.com>
2016-11-01 20:50:45 +0100

    trying to make it faster

--- NearestNeighbor.cpp
+++ NearestNeighbor.cpp
@@ -27,17 +27,6 @@ vector<int> nearestNeighbor(Cities &cities){
     // put in the first city and delete it from cities
     tour.push_back(thisvertex);
 
-    /*   tour[0] = 0
-   used[0] = true
-   for i = 1 to n-1
-      best = -1
-      for j = 0 to n-1
-         if not used[j] and (best = -1 or dist(tour[i-1],j) < dist(tour[i-1],best))
-            best = j
-      tour[i] = best
-      used[best] = true
-   return tour */
-
 
     // some variables to keep track of best
     int best = numeric_limits<int>::max();
